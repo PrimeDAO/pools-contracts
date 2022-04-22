d7afa32a49b178f9866a3c50dd33925ec13fbd97
Francis Perez <francistp11@gmail.com>
2022-04-22 10:09:49 -0400

    saving work, all contracts will compile. Need to add testing files and fix logic

---
+++ contracts/utils/MathUtil.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// copied from https://github.com/Uniswap/v2-core/blob/master/contracts/libraries/SafeMath.sol
+
+/**
+ * @dev Standard math utilities missing in the Solidity language.
+ */
+library MathUtil {
+    /**
+     * @dev Returns the smallest of two numbers.
+     */
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    
+}
