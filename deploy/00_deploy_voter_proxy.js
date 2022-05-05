4ff10ce1d4a188005d729e40f96531dee9dcdfa3
decadanse <gertesset@gmail.com>
2022-05-05 12:19:51 +0300

    VoterProxy info added; uncomment after merging with branch with VoterProxy contract

--- deploy/00_deploy_voter_proxy.js
+++ deploy/00_deploy_voter_proxy.js
@@ -4,9 +4,23 @@
 //   // const safeInstance =
 //   //   network.name == "kovan" ? root : await ethers.getContract("VoterProxy");
 
+//   let minter;
+//   let bal = await ethers.getContract("D2DToken");
+//   let veBal = await deploy("veBalMock", {
+//     from: root,
+//     args: [
+//       token_addr,
+//       "veBalMock",
+//       "VBM",
+//       _authorizer_adaptor,
+//     ],
+//     log: true,
+//   });
+//   let gaugeController;
+
 //   const { address: seedAddress } = await deploy("VoterProxy", {
 //     from: root,
-//     args: [],
+//     args: [minter, bal, veBal, gaugeController],
 //     log: true,
 //   });
 // };
