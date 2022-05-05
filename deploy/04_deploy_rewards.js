aef6788ecec8f05c4a9c3e9afe894bbe1a130967
decadanse <gertesset@gmail.com>
2022-05-05 11:32:25 +0300

    edited

--- deploy/04_deploy_rewards.js
+++ deploy/04_deploy_rewards.js
@@ -5,7 +5,7 @@ const deployFunction = async ({ getNamedAccounts, deployments, network }) => {
   
   const pid = 1; //1 for example (set correct later_)
   const stakingTokenInstance = await ethers.getContract("D2DToken"); //Staking token provided will be a d2dBAL, minted in the Deposit contract.
-  const rewardToken = '0xC128a9954e6c874eA3d62ce62B468bA073093F25'; //reward contract is going to be BAL, which will be received from BAL ve model.
+  const rewardToken = await ethers.getContract("veBalMock"); //reward contract is going to be BAL, which will be received from BAL ve model.
   const operator = await ethers.getContract("Controller");
   const rewardManager = await ethers.getContract("Controller");
 
