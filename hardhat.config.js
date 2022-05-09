11200b206cafb0aa13d9ed8ac3f4d5dc9e294d40
bxmmm1 <benjaminxh+github@gmail.com>
2022-05-09 13:35:48 +0200

    Remove other compilers

--- hardhat.config.js
+++ hardhat.config.js
@@ -95,33 +95,6 @@ module.exports = {
           },
         },
       },
-      {
-        version: "0.8.10",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: "0.8.9",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: "0.8.6",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
     ],
   },
   etherscan: {
