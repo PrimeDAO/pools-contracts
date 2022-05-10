91eed34388f7eaa910e0d644102955e25623e27d
BritikovKI <BritikovKI@yandex.ru>
2022-05-10 21:49:46 +0200

    lint fix

--- deploy/01_deploy_voter_proxy.js
+++ deploy/01_deploy_voter_proxy.js
@@ -4,7 +4,6 @@ const deployFunction = async ({ getNamedAccounts, deployments, ethers }) => {
   // const safeInstance =
   //   network.name == "kovan" ? root : await ethers.getContract("VoterProxy");
 
-  let minter;
   let bal = await ethers.getContract("D2DToken");
   let veBal = await deploy("veBalMock", {
     from: root,
@@ -16,9 +15,8 @@ const deployFunction = async ({ getNamedAccounts, deployments, ethers }) => {
     ],
     log: true,
   });
-  let gaugeController;
 
-  const { address: seedAddress } = await deploy("VoterProxy", {
+  await deploy("VoterProxy", {
     from: root,
     args: ["0xEFc3a819695932394D89b8AF6f49e0D89EDf9A40", bal.address, veBal.address, "0xEFc3a819695932394D89b8AF6f49e0D89EDf9A40"],// TODO: change with minter and gauge controller
     log: true,
