6c95f6d103eb40dbe162e43380cf910ef9ae895d
bxmmm1 <benjaminxh+github@gmail.com>
2022-05-11 15:05:23 +0200

    Refactor BaseRewardPool
    Add custom errors, NatSpec

--- contracts/BaseRewardPool.sol
+++ contracts/BaseRewardPool.sol
@@ -4,37 +4,44 @@ pragma solidity ^0.8.13;
 import "./utils/Interfaces.sol";
 import "./utils/MathUtil.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
+/// @title Base Reward Pool contract
+/// @dev Rewards contract for Prime Pools is based on the convex contract
 contract BaseRewardPool {
-    IERC20 public rewardToken;
-    IERC20 public stakingToken;
-    uint256 public constant DURATION = 7 days;
+    event RewardAdded(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
 
-    address public operator;
-    address public rewardManager;
+    error Unauthorized();
+    error InvalidAmount();
+
+    uint256 public constant DURATION = 7 days;
+    uint256 public constant NEW_REWARD_RATIO = 830;
+
+    // Rewards token is Bal.
+    IERC20 public immutable rewardToken;
+    //Staking token is d2dBal.
+    IERC20 public immutable stakingToken;
+
+    address public immutable operator;
+    address public immutable rewardManager;
 
     uint256 public pid;
-    uint256 public periodFinish = 0;
-    uint256 public rewardRate = 0;
+    uint256 public periodFinish;
+    uint256 public rewardRate;
     uint256 public lastUpdateTime;
     uint256 public rewardPerTokenStored;
-    uint256 public queuedRewards = 0;
-    uint256 public currentRewards = 0;
-    uint256 public historicalRewards = 0;
-    uint256 public constant newRewardRatio = 830;
+    uint256 public queuedRewards;
+    uint256 public currentRewards;
+    uint256 public historicalRewards;
     uint256 private _totalSupply;
+
     mapping(address => uint256) public userRewardPerTokenPaid;
     mapping(address => uint256) public rewards;
     mapping(address => uint256) private _balances;
 
     address[] public extraRewards;
-
-    event RewardAdded(uint256 reward);
-    event Staked(address indexed user, uint256 amount);
-    event Withdrawn(address indexed user, uint256 amount);
-    event RewardPaid(address indexed user, uint256 reward);
 
     constructor(
         uint256 pid_,
@@ -44,35 +51,10 @@ contract BaseRewardPool {
         address rewardManager_
     ) public {
         pid = pid_;
-        stakingToken = IERC20(stakingToken_); //Staking token is d2dBal.
-        rewardToken = IERC20(rewardToken_); // Rewards token is Bal.
+        stakingToken = IERC20(stakingToken_);
+        rewardToken = IERC20(rewardToken_);
         operator = operator_;
         rewardManager = rewardManager_;
-    }
-
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
-    }
-
-    function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
-    }
-
-    function extraRewardsLength() external view returns (uint256) {
-        return extraRewards.length;
-    }
-
-    function addExtraReward(address _reward) external returns (bool) {
-        require(msg.sender == rewardManager, "!authorized");
-        require(_reward != address(0), "!reward setting");
-
-        extraRewards.push(_reward);
-        return true;
-    }
-
-    function clearExtraRewards() external {
-        require(msg.sender == rewardManager, "!authorized");
-        delete extraRewards;
     }
 
     modifier updateReward(address account) {
@@ -85,10 +67,59 @@ contract BaseRewardPool {
         _;
     }
 
+    modifier onlyAddress(address authorizedAddress) {
+        if (msg.sender != authorizedAddress) {
+            revert Unauthorized();
+        }
+        _;
+    }
+
+    /// @notice Returns total supply
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /// @notice Get the specified address' balance
+    /// @param account The address of the token holder
+    /// @return The `account`'s balance
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    /// @notice Returns number of extra rewards
+    function extraRewardsLength() external view returns (uint256) {
+        return extraRewards.length;
+    }
+
+    /// @notice Adds an extra reward
+    /// @dev only `rewardManager` can add extra rewards
+    /// @param _reward token address of the reward
+    /// @return true on success
+    function addExtraReward(address _reward)
+        external
+        onlyAddress(rewardManager)
+        returns (bool)
+    {
+        require(_reward != address(0), "!reward setting");
+        extraRewards.push(_reward);
+        return true;
+    }
+
+    /// @notice Clears all extra rewards
+    /// @dev only `rewardManager` can clear extra rewards
+    function clearExtraRewards() external onlyAddress(rewardManager) {
+        delete extraRewards;
+    }
+
+    /// @notice Returns last time reward applicable
+    /// @return The lower value of current block.timestamp or last time reward applicable
     function lastTimeRewardApplicable() public view returns (uint256) {
+        // solhint-disable-next-line
         return MathUtil.min(block.timestamp, periodFinish);
     }
 
+    /// @notice Returns rewards per token staked
+    /// @return The rewards per token staked
     function rewardPerToken() public view returns (uint256) {
         if (totalSupply() == 0) {
             return rewardPerTokenStored;
@@ -100,6 +131,9 @@ contract BaseRewardPool {
                 1e18) / totalSupply());
     }
 
+    /// @notice Returns the `account`'s earned rewards
+    /// @param account The address of the token holder
+    /// @return The `account`'s earned rewards
     function earned(address account) public view returns (uint256) {
         return
             (balanceOf(account) *
@@ -108,17 +142,19 @@ contract BaseRewardPool {
             rewards[account];
     }
 
+    /// @notice Stakes `amount` tokens
+    /// @param _amount The amount of tokens user wants to stake
+    /// @return true on success
     function stake(uint256 _amount)
         public
         updateReward(msg.sender)
         returns (bool)
     {
-        require(_amount > 0, "RewardPool : Cannot stake 0");
-
-        //also stake to linked rewards
-        for (uint256 i = 0; i < extraRewards.length; i++) {
-            IRewards(extraRewards[i]).stake(msg.sender, _amount);
+        if (_amount < 1) {
+            revert InvalidAmount();
         }
+
+        stakeToExtraRewards(msg.sender, _amount);
 
         _totalSupply = _totalSupply + (_amount);
         _balances[msg.sender] = _balances[msg.sender] + (_amount);
@@ -129,95 +165,141 @@ contract BaseRewardPool {
         return true;
     }
 
+    /// @dev Gas optimization for loops that iterate over extra rewards
+    /// We know that this can't overflow because we can't interate over big arrays
+    function unsafeInc(uint256 x) internal pure returns (uint256) {
+        unchecked {
+            return x + 1;
+        }
+    }
+
+    /// @dev Stakes `amount` tokens for address `for` to extra rewards tokens
+    /// RewardManager `rewardManager` is responsible for adding reward tokens
+    /// @param _for Who are we staking for
+    /// @param _amount The amount of tokens user wants to stake
+    function stakeToExtraRewards(address _for, uint256 _amount) internal {
+        address[] memory extraRewardsMemory = extraRewards;
+        for (uint256 i = 0; i < extraRewardsMemory.length; i = unsafeInc(i)) {
+            IRewards(extraRewardsMemory[i]).stake(_for, _amount);
+        }
+    }
+
+    /// @dev Stakes `amount` tokens for address `for` to extra rewards tokens
+    /// RewardManager `rewardManager` is responsible for adding reward tokens
+    /// @param _for Who are we staking for
+    /// @param _amount The amount of tokens user wants to stake
+    function withdrawExtraRewards(address _for, uint256 _amount) internal {
+        address[] memory extraRewardsMemory = extraRewards;
+        for (uint256 i = 0; i < extraRewardsMemory.length; i = unsafeInc(i)) {
+            IRewards(extraRewardsMemory[i]).withdraw(_for, _amount);
+        }
+    }
+
+    /// @notice Stakes all BAL tokens
+    /// @return true on success
     function stakeAll() external returns (bool) {
         uint256 balance = stakingToken.balanceOf(msg.sender);
         stake(balance);
         return true;
     }
 
-    //here we are locking tokens
+    /// @notice Stakes `amount` tokens for `_for`
+    /// @param _for Who are we staking for
+    /// @param _amount The amount of tokens user wants to stake
     function stakeFor(address _for, uint256 _amount)
         public
         updateReward(_for)
         returns (bool)
     {
-        require(_amount > 0, "RewardPool : Cannot stake 0");
-
-        //also stake to linked rewards
-        for (uint256 i = 0; i < extraRewards.length; i++) {
-            IRewards(extraRewards[i]).stake(_for, _amount);
+        if (_amount < 1) {
+            revert InvalidAmount();
         }
 
-        //give to _for
+        stakeToExtraRewards(_for, _amount);
+
         _totalSupply = _totalSupply + (_amount);
+        // update _for balances
         _balances[_for] = _balances[_for] + (_amount);
 
-        //take away from sender
+        // take away from sender
         stakingToken.transferFrom(msg.sender, address(this), _amount);
         emit Staked(_for, _amount);
 
         return true;
     }
 
-    function withdraw(uint256 amount, bool claim)
+    /// @notice Unstakes `amount` tokens
+    /// @param _amount The amount of tokens that the user wants to withdraw
+    /// @param _claim Whether or not the user wants to claim their rewards
+    function withdraw(uint256 _amount, bool _claim)
         public
         updateReward(msg.sender)
         returns (bool)
     {
-        require(amount > 0, "RewardPool : Cannot withdraw 0");
-
-        //also withdraw from linked rewards
-        for (uint256 i = 0; i < extraRewards.length; i++) {
-            IRewards(extraRewards[i]).withdraw(msg.sender, amount);
+        if (_amount < 1) {
+            revert InvalidAmount();
         }
 
-        _totalSupply = _totalSupply - (amount);
-        _balances[msg.sender] = _balances[msg.sender] - (amount); //(_balances created with mapping)
+        // withdraw from linked rewards
+        withdrawExtraRewards(msg.sender, _amount);
 
-        stakingToken.transfer(msg.sender, amount);
-        emit Withdrawn(msg.sender, amount);
+        _totalSupply = _totalSupply - (_amount);
+        _balances[msg.sender] = _balances[msg.sender] - (_amount);
 
-        if (claim) {
+        // return staked tokens to sender
+        stakingToken.transfer(msg.sender, _amount);
+        emit Withdrawn(msg.sender, _amount);
+
+        // claim staking rewards
+        if (_claim) {
             getReward(msg.sender, true);
         }
 
         return true;
     }
 
-    //It is possible to withdraw the whole amount of d2dBal.
-    function withdrawAll() external {
-        bool claim = true;
-        withdraw(_balances[msg.sender], claim);
+    /// @notice Withdraw all tokens
+    function withdrawAll(bool _claim) external {
+        withdraw(_balances[msg.sender], _claim);
     }
 
-    function withdrawAndUnwrap(uint256 amount, bool claim)
+    /// @notice Withdraw `amount` tokens and unwrap
+    /// @param _amount The amount of tokens that the user wants to withdraw
+    /// @param _claim Whether or not the user wants to claim their rewards
+    function withdrawAndUnwrap(uint256 _amount, bool _claim)
         public
         updateReward(msg.sender)
         returns (bool)
     {
-        //also withdraw from linked rewards
-        for (uint256 i = 0; i < extraRewards.length; i++) {
-            IRewards(extraRewards[i]).withdraw(msg.sender, amount);
+        if (_amount < 1) {
+            revert InvalidAmount();
         }
 
-        _totalSupply = _totalSupply - (amount);
-        _balances[msg.sender] = _balances[msg.sender] - (amount);
+        withdrawExtraRewards(msg.sender, _amount);
 
-        //tell operator to withdraw from here directly to user
-        IDeposit(operator).withdrawTo(pid, amount, msg.sender);
-        emit Withdrawn(msg.sender, amount);
+        _totalSupply = _totalSupply - (_amount);
+        _balances[msg.sender] = _balances[msg.sender] - (_amount);
+
+        // tell operator to withdraw from here directly to user
+        IDeposit(operator).withdrawTo(pid, _amount, msg.sender);
+        emit Withdrawn(msg.sender, _amount);
 
         //get rewards too
-        if (claim) {
+        if (_claim) {
             getReward(msg.sender, true);
         }
         return true;
     }
 
-    function withdrawAllAndUnwrap(bool claim) external {
-        withdrawAndUnwrap(_balances[msg.sender], claim);
+    /// @notice Withdraw all tokens and unwrap
+    /// @param _claim Whether or not the user wants to claim their rewards
+    function withdrawAllAndUnwrap(bool _claim) external {
+        withdrawAndUnwrap(_balances[msg.sender], _claim);
     }
 
+    /// @notice Claims Rewards for `_account`
+    /// @param _account The account to claim rewards for
+    /// @param _claimExtras Whether or not the user wants to claim extra rewards
     function getReward(address _account, bool _claimExtras)
         public
         updateReward(_account)
@@ -231,44 +313,60 @@ contract BaseRewardPool {
             emit RewardPaid(_account, reward);
         }
 
-        //also get rewards from linked rewards
+        // also get rewards from linked rewards
         if (_claimExtras) {
-            for (uint256 i = 0; i < extraRewards.length; i++) {
-                IRewards(extraRewards[i]).getReward(_account);
+            address[] memory extraRewardsMemory = extraRewards;
+            for (
+                uint256 i = 0;
+                i < extraRewardsMemory.length;
+                i = unsafeInc(i)
+            ) {
+                IRewards(extraRewardsMemory[i]).getReward(_account);
             }
         }
         return true;
     }
 
+    /// @notice Claims Reward for signer
+    /// @return true on success
     function getReward() external returns (bool) {
         getReward(msg.sender, true);
         return true;
     }
 
+    /// @notice Donates reward token to this contract
+    /// @param _amount The amount of tokens to donate
+    /// @return true on success
     function donate(uint256 _amount) external returns (bool) {
         IERC20(rewardToken).transferFrom(msg.sender, address(this), _amount);
         queuedRewards = queuedRewards + _amount;
+        return true;
     }
 
-    function queueNewRewards(uint256 _rewards) external returns (bool) {
-        require(msg.sender == operator, "!authorized");
-
+    /// @notice Queue new rewards
+    /// @dev Only the operator can queue new rewards
+    /// @param _rewards The amount of tokens to queue
+    /// @return true on success
+    function queueNewRewards(uint256 _rewards)
+        external
+        onlyAddress(operator)
+        returns (bool)
+    {
         _rewards = _rewards + queuedRewards;
 
+        // solhint-disable-next-line
         if (block.timestamp >= periodFinish) {
             notifyRewardAmount(_rewards);
             queuedRewards = 0;
             return true;
         }
 
-        //et = now - (finish-DURATION)
+        // solhint-disable-next-line
         uint256 elapsedTime = block.timestamp - (periodFinish - DURATION);
-        //current at now: rewardRate * elapsedTime
         uint256 currentAtNow = rewardRate * elapsedTime;
         uint256 queuedRatio = (currentAtNow * 1000) / _rewards;
 
-        //uint256 queuedRatio = currentRewards.mul(1000).div(_rewards);
-        if (queuedRatio < newRewardRatio) {
+        if (queuedRatio < NEW_REWARD_RATIO) {
             notifyRewardAmount(_rewards);
             queuedRewards = 0;
         } else {
@@ -282,16 +380,20 @@ contract BaseRewardPool {
         updateReward(address(0))
     {
         historicalRewards = historicalRewards + reward;
+        // solhint-disable-next-line
         if (block.timestamp >= periodFinish) {
             rewardRate = reward / DURATION;
         } else {
+            // solhint-disable-next-line
             uint256 remaining = periodFinish - block.timestamp;
             uint256 leftover = remaining * rewardRate;
             reward = reward + leftover;
             rewardRate = reward / DURATION;
         }
         currentRewards = reward;
+        // solhint-disable-next-line
         lastUpdateTime = block.timestamp;
+        // solhint-disable-next-line
         periodFinish = block.timestamp + DURATION;
         emit RewardAdded(reward);
     }
