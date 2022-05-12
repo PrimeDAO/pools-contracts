f7ad34ccf16e932e268b07b113fed0997e617db8
kin <kinspotting@protonmail.com>
2022-05-12 20:51:45 +0200

    check constructor

--- test/unit/bal-depositor.js
+++ test/unit/bal-depositor.js
@@ -24,11 +24,24 @@ describe("Contract: BalDepositor", async () => {
   });
   // first deployment test
   it("checks if deployed contracts are ZERO_ADDRESS", async () => {
+    assert(setup.balDepositor.address != constants.ZERO_ADDRESS);
     assert(setup.tokenInstances.WethBal.address != constants.ZERO_ADDRESS);
     assert(setup.tokenInstances.D2DToken.address != constants.ZERO_ADDRESS);
     assert(setup.tokenInstances.VeBal.address != constants.ZERO_ADDRESS);
+  });
+
+  it("checks BalDepositor construtor", async () => {
     assert(setup.balDepositor.address != constants.ZERO_ADDRESS);
 
+    const wethBalAdress = await setup.balDepositor.wethBal();
+    const staker = await setup.balDepositor.staker();
+    const minter = await setup.balDepositor.minter();
+    const escrow =  await setup.balDepositor.escrow();
+
+    assert(wethBalAdress == setup.tokenInstances.WethBal.address);
+    assert(staker == setup.roles.staker.address);
+    assert(minter == setup.tokenInstances.D2DToken.address);
+    assert(escrow == setup.tokenInstances.VeBal.address);
     });
   });
 });
