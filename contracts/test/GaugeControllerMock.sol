181dbb03a2347e30ab3bab99830b2c44bfdf6d6f
decadanse <gertesset@gmail.com>
2022-05-13 11:39:46 +0300

    functions

--- contracts/test/GaugeControllerMock.sol
+++ contracts/test/GaugeControllerMock.sol
@@ -13,16 +13,24 @@ interface VotingEscrow {
 }   
 
 contract GaugeControllerMock {
+
     struct Point{
         uint256 bias;
         uint256 slope;
     }
 
-    struct LockedBalance{
+    struct VotedSlope{
         uint256 slope; 
         uint256 power;
         uint256 end;
     }  
+
+    // 7 * 86400 seconds - all future times are rounded by week
+    uint256 constant WEEK = 604800;
+    // Cannot change weight votes more often than once in 10 days
+    uint256 constant WEIGHT_VOTE_DELAY = 10 * 86400;
+    uint256 constant MULTIPLIER = 10 ** 18;
+    address constant ZERO_ADDRESS = address(0x0000000000000000000000000000000000000000);
 
     event CommitOwnership(address admin);
     event ApplyOwnership(address admin);
@@ -31,8 +39,6 @@ contract GaugeControllerMock {
     event NewGaugeWeight(address gauge_address, uint256 time, uint256 weight, uint256 total_weight);
     event VoteForGauge(uint256 time, address user, address gauge_addr, uint256 weight);
     event NewGauge(address addr, int128 gauge_type, uint256 weight);
-
-    uint256 constant MULTIPLIER = 10 ** 18;
 
     address public admin;
     address public future_admin;
@@ -71,7 +77,251 @@ contract GaugeControllerMock {
     uint256[1000000000] public time_type_weight; // type_id -> last scheduled time (next week)
 
 
+    //__init__
+    constructor(
+        address _token,
+        address _voting_escrow
+    ) public {
+        /**
+        @notice Contract constructor
+        @param _token `ERC20CRV` contract address
+        @param _voting_escrow `VotingEscrow` contract address
+        */
+        require(_token != ZERO_ADDRESS, "GaugeControllerMock: _token == ZERO_ADDRESS");
+        require(_voting_escrow != ZERO_ADDRESS, "GaugeControllerMock: _voting_escrow == ZERO_ADDRESS");
 
+        admin = msg.sender;
+        token = _token;
+        voting_escrow = _voting_escrow;
+        time_total = block.timestamp / WEEK * WEEK;
+    }
+
+    function commit_transfer_ownership(address addr) external {
+        /**
+        @notice Transfer ownership of GaugeController to `addr`
+        @param addr Address to have ownership transferred to
+        */
+        require(msg.sender == admin, "GaugeControllerMock: admin only"); //dev: admin only
+        future_admin = addr;
+        emit CommitOwnership(addr);
+    }
+    function apply_transfer_ownership() external {
+        /**
+        @notice Apply pending ownership transfer
+        */
+        require(msg.sender == admin, "GaugeControllerMock: admin only"); //dev: admin only
+        address _admin = future_admin;
+        require(_admin != ZERO_ADDRESS, "GaugeControllerMock: _admin != ZERO_ADDRESS"); //dev: admin only
+        admin = _admin;
+        emit ApplyOwnership(_admin);
+    }
+    function gauge_types(address _addr) external view returns (int128) {
+        /**
+        @notice Get gauge type for address
+        @param _addr Gauge address
+        @return Gauge type id
+        */
+        int128 gauge_type = gauge_types_[_addr];
+        require(gauge_type != 0, "GaugeControllerMock: gauge_type != 0");
+
+        return gauge_type - 1;
+    }
+    function _get_type_weight(int128 gauge_type) internal returns (uint256) {
+        /**
+        @notice Fill historic type weights week-over-week for missed checkins
+                and return the type weight for the future week
+        @param gauge_type Gauge type id
+        @return Type weight
+        */
+        uint256 t = time_type_weight[gauge_type];
+        if (t > 0) {
+            uint256 w = points_type_weight[gauge_type][t];
+            for (uint256 i = 0; i < 500; i++){
+                if (t > block.timestamp){
+                    break;
+                }
+                t += WEEK;
+                points_type_weight[gauge_type][t] = w;
+                if (t > block.timestamp){
+                    time_type_weight[gauge_type] = t;
+                }
+            }
+            return w;
+        } else{
+            return 0;
+        }
+    }
+    function _get_sum(int128 gauge_type) internal returns (uint256) {
+        /**
+        @notice Fill sum of gauge weights for the same type week-over-week for
+                missed checkins and return the sum for the future week
+        @param gauge_type Gauge type id
+        @return Sum of weights
+        */
+        uint256 t = time_sum[gauge_type];
+        if (t > 0) {
+            Point pt = points_sum[gauge_type][t];
+            for (uint256 i = 0; i < 500; i++){
+                if (t > block.timestamp) {
+                    break;
+                }
+                t += WEEK;
+                uint256 d_bias = pt.slope * WEEK;
+                if (pt.bias > d_bias) {
+                    pt.bias -= d_bias;
+                    uint256 d_slope = self.changes_sum[gauge_type][t];
+                    pt.slope -= d_slope;
+                } else {
+                    pt.bias = 0;
+                    pt.slope = 0;
+                }
+                points_sum[gauge_type][t] = pt;
+                if (t > block.timestamp) {
+                    time_sum[gauge_type] = t;
+                }
+            }
+            return pt.bias;
+        } else {
+            return 0;
+        }
+    }
+    function _get_total() internal returns (uint256) {
+        /**
+        @notice Fill historic total weights week-over-week for missed checkins
+                and return the total for the future week
+        @return Total weight
+        */
+        uint256 t = time_total;
+        int128 _n_gauge_types = n_gauge_types;
+
+        if (t > block.timestamp) {
+            // If we have already checkpointed - still need to change the value
+            t -= WEEK;            
+        }
+        uint256 pt = points_total[t];
+
+        for (uint256 gauge_type = 0; gauge_type < 100; gauge_type++){
+            if (gauge_type == _n_gauge_types) {
+                break;
+            }
+            _get_sum(gauge_type);
+            _get_type_weight(gauge_type);
+        }
+
+        for (uint256 i = 0; i < 500; i++){
+            if (t > block.timestamp) {
+                break;
+            }
+            t += WEEK;
+            pt = 0;
+            // Scales as n_types * n_unchecked_weeks (hopefully 1 at most)
+            for (uint256 gauge_type = 0; gauge_type < 100; gauge_type++){
+                if (gauge_type == _n_gauge_types) {
+                    break;
+                }
+                uint256 type_sum = points_sum[gauge_type][t].bias;
+                uint256 type_weight = points_type_weight[gauge_type][t];
+                pt += type_sum * type_weight;
+            }
+            points_total[t] = pt;
+
+            if (t > block.timestamp) {
+                time_total = t;
+            }
+        }
+        return pt;
+    }
+    function _get_weight(address gauge_addr) internal returns (uint256) {
+        /**
+        @notice Fill historic gauge weights week-over-week for missed checkins
+                and return the total for the future week
+        @param gauge_addr Address of the gauge
+        @return Gauge weight
+        */
+        uint256 t = time_weight[gauge_addr];
+
+        if (t > 0) {
+            Point pt = points_weight[gauge_addr][t];
+            for (uint256 i = 0; i < 500; i++){
+                if (t > block.timestamp) {
+                    break;
+                }
+                t += WEEK;
+                uint256 d_bias = pt.slope * WEEK;
+                // Scales as n_types * n_unchecked_weeks (hopefully 1 at most)
+                if (pt.bias > d_bias) {
+                    pt.bias -= d_bias;
+                    uint256 d_slope = changes_weight[gauge_addr][t];
+                    pt.slope -= d_slope;
+                } else {
+                    pt.bias = 0;
+                    pt.slope = 0;
+                }
+                points_weight[gauge_addr][t] = pt;
+                if (t > block.timestamp) {
+                    time_weight[gauge_addr] = t;
+                }
+            }
+            return pt.bias;
+        } else {
+            return 0;
+        }
+    }
+
+    // in original code uint256 weight = 0 by default
+    // but Solidity does not support default parameters
+    function add_gauge(address addr, int256 gauge_type, uint256 weight) external {
+        /**
+        @notice Add gauge `addr` of type `gauge_type` with weight `weight`
+        @param addr Gauge address
+        @param gauge_type Gauge type
+        @param weight Gauge weight
+        */
+        require(msg.sender == admin, "GaugeControllerMock: msg.sender == admin,");
+        require((gauge_type >= 0) && (gauge_type < n_gauge_types), "GaugeControllerMock: (gauge_type >= 0) && (gauge_type < n_gauge_types)");
+        require(gauge_types_[addr] == 0, "GaugeControllerMock: gauge_types_[addr] == 0"); //dev: cannot add the same gauge twice
+
+        int128 n = n_gauges;
+        n_gauges = n + 1;
+        gauges[n] = addr;
+
+        gauge_types_[addr] = gauge_type + 1;
+        uint256 next_time = (block.timestamp + WEEK) / WEEK * WEEK;
+
+        if (weight > 0) {
+            uint256 _type_weight = _get_type_weight(gauge_type);
+            uint256 _old_sum = _get_sum(gauge_type);
+            uint256 _old_total = _get_total();
+
+            points_sum[gauge_type][next_time].bias = weight + _old_sum;
+            time_sum[gauge_type] = next_time;
+            points_total[next_time] = _old_total + _type_weight * weight;
+            time_total = next_time;
+
+            points_weight[addr][next_time].bias = weight;
+        }
+
+        if (time_sum[gauge_type] == 0) {
+            time_sum[gauge_type] = next_time;
+        }
+        time_weight[addr] = next_time;
+
+        emit NewGauge(addr, gauge_type, weight);
+    }
+    function checkpoint() external {
+        /**
+        @notice Checkpoint to fill data common for all gauges
+        */
+        _get_total();
+    }
+    function checkpoint_gauge(address addr) external {
+        /**
+        @notice Checkpoint to fill data for both a specific gauge and common for all gauges
+        @param addr Gauge address
+        */
+        _get_weight(addr);
+        _get_total();
+    }
 
 
 
