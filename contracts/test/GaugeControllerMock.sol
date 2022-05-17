8a3a2d62af16b90f99390d818481472e6147514f
decadanse <gertesset@gmail.com>
2022-05-17 17:27:23 +0300

    comments removed

--- contracts/test/GaugeControllerMock.sol
+++ contracts/test/GaugeControllerMock.sol
@@ -1,11 +1,6 @@
-// TODO Mock a contract without removing any of it's functionality
-//by address 0x2f50d538606fa9edd2b11e2446beb18c9d5846bb
-
 // solium-disable linebreak-style
 pragma solidity 0.8.13;
 
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 interface VotingEscrow {
     function get_last_user_slope(address addr) external view returns (uint256);
     function locked__end(address addr) external view returns (uint256);
