a2f7f9d221380de9802443a1548f55efe6e934ae
decadanse <gertesset@gmail.com>
2022-05-17 15:42:07 +0300

    test files upd

---
+++ test/Controller-test.js
@@ -0,0 +1,204 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { expectRevert } = require("@openzeppelin/test-helpers");
+// const {
+//   utils: { parseEther, parseUnits },
+//   BigNumber,
+// } = ethers;
+
+const init = require("./test-init.js");
+
+const deploy = async () => {
+  const setup = await init.initialize(await ethers.getSigners());
+
+  setup.tokens = await init.getTokens(setup);
+  
+  setup.VoterProxy = await init.getVoterProxy(setup);
+
+  setup.controller = await init.controller(setup);
+
+  setup.baseRewardPool = await init.baseRewardPool(setup);
+
+  setup.rewardFactory = await init.rewardFactory(setup);
+
+//   setup.proxyFactory = await init.proxyFactory(setup);
+
+  setup.stashFactory = await init.stashFactory(setup);
+
+  setup.data = {};
+
+  return setup;
+};
+
+// const getDecimals = async (token) => await token.decimals();
+
+// const getTokenAmount = (tokenDecimal) => (amount) =>
+//     parseUnits(amount, tokenDecimal.toString());
+
+describe("Contract: Controller", async () => {
+  let setup;
+  let root;
+
+  let platformFee;
+  let profitFee;
+  let pid;
+  let rewardFactory;
+  let stashFactory;
+  let tokenFactory;
+  let lptoken;
+  let gauge;
+  let stashVersion;
+
+  //constants
+//   const zero_address = "0x0000000000000000000000000000000000000000";
+
+  context("» creator is avatar", () => {
+    before("!! setup", async () => {
+      setup = await deploy();
+
+
+      // // Roles
+      root = setup.roles.root;
+      staker = setup.roles.staker;
+      admin = setup.roles.prime;
+      reward_manager = setup.roles.reward_manager;
+      authorizer_adaptor = setup.roles.authorizer_adaptor;
+      operator = setup.roles.operator;
+
+      platformFee = 500;
+      profitFee = 100;
+      //TODO: fill () with arguments
+    //   await setup.controller.setOwner();
+    //   await setup.controller.setPoolManager();
+    //   await setup.controller.setFactories();
+    //   await setup.controller.setArbitrator();
+    //   await setup.controller.setVoteDelegate();
+    //   await setup.controller.setRewardContracts();
+    //   await setup.controller.setFeeInfo();
+    // //   await setup.controller.setFees(); - test below
+    //   await setup.controller.setTreasury();
+
+    });
+
+    context("» Testing changed functions", () => {
+        context("» setFeeInfo testing", () => {
+            // it("Checks feeToken", async () => {
+            //     expect((await setup.controller.feeToken()).toString()).to.equal(zero_address);
+            //     await setup.controller
+            //             .connect(root)
+            //             .setFeeInfo(); //crashed with "Error: Transaction reverted: function returned an unexpected amount of data"
+            //             //reason of error - not this issue; it should be solved in "Change Curve interactions with Balancer interactions" issue
+            //     expectRevert((await setup.controller.feeToken()).toString()).to.equal(zero_address);
+            // });
+        });
+        context("» setFees testing", () => {
+            it("Sets correct fees", async () => {
+                await setup.controller
+                        .connect(root)
+                        .setFees(platformFee, profitFee);            
+            });
+            it("Should fail if total >MaxFees", async () => {
+                platformFee = 1000;
+                profitFee = 1001;
+                //MaxFees = 2000;
+                await expectRevert(
+                    setup.controller
+                        .connect(root)
+                        .setFees(platformFee, profitFee),
+                    ">MaxFees"
+                );                
+            });
+            it("Should fail if platformFee is too small", async () => {
+                platformFee = 400;
+                profitFee = 100;
+                await setup.controller
+                        .connect(root)
+                        .setFees(platformFee, profitFee);
+                expect((await setup.controller.platformFees()).toString()).to.equal("500");              
+            });
+            it("Should fail if platformFee is too big", async () => {
+                platformFee = 10000;
+                profitFee = 100;
+                await expectRevert(
+                    setup.controller
+                        .connect(root)
+                        .setFees(platformFee, profitFee),
+                    ">MaxFees"
+                );  
+            });
+            it("Should fail if profitFee is too small", async () => {
+                platformFee = 500;
+                profitFee = 10;
+                await setup.controller
+                        .connect(root)
+                        .setFees(platformFee, profitFee);
+                expect((await setup.controller.profitFees()).toString()).to.equal("100");
+
+            });
+            it("Should fail if profitFee is too big", async () => {
+                platformFee = 500;
+                profitFee = 1000;
+                await setup.controller
+                        .connect(root)
+                        .setFees(platformFee, profitFee);
+                expect((await setup.controller.profitFees()).toString()).to.equal("100");
+
+            });
+        });
+        context("» _earmarkRewards testing", () => {
+            it("Calls earmarkRewards with non existing pool number", async () => {
+                pid = 1;
+                await expectRevert(
+                    setup.controller
+                        .connect(root)
+                        .earmarkRewards(pid),
+                    "Controller: pool is not exists"
+                );  
+            });
+            it("Sets VoterProxy operator ", async () => {
+                expect(await setup.VoterProxy.connect(root).setOperator(setup.controller.address));
+            });
+            it("Sets factories", async () => { //not this issue; now it is
+                rewardFactory = setup.rewardFactory;
+                stashFactory = setup.stashFactory; //stash to handle extra incentives
+                tokenFactory = setup.tokens.TokenFactory; //create a tokenized deposit //booster tokenFactory https://etherscan.io/address/0x3c995e43e6ddd551e226f4c5544c77bfed147ab9                
+                expect(await setup.controller.connect(root).setFactories(rewardFactory.address, stashFactory.address, tokenFactory.address));
+                // Error: VM Exception while processing transaction: reverted with reason string '!auth'
+                // at RewardFactory.CreateBalRewards 
+            });
+            it("Sets VoterProxy as StashFactory implementation ", async () => {
+                expect(await setup.stashFactory.connect(root).setImplementation(setup.VoterProxy.address, setup.VoterProxy.address, setup.VoterProxy.address));
+            });
+            // it("Adds pool", async () => { //not this issue; now it is
+            //     //lp token is unwithdrawable --> PoolToken
+            //     lptoken = setup.tokens.PoolContract; //address; 
+            //     //reward contract is going to be BAL
+            //     gauge = setup.tokens.GaugeController;// gauge controller Mock //https://dev.balancer.fi/resources/vebal-and-gauges/gauges
+            //     stashVersion = 1; //uint256
+            //     await setup.controller.connect(root).addPool(lptoken.address, gauge.address, stashVersion);
+            //     expect(
+            //         (await setup.controller.poolLength()).toNumber()
+            //     ).to.equal(1);
+            //     // expect(
+            //     //     await tokenFactory.token()
+            //     // ).to.equal(1);
+            //     // expect(
+            //     //     await tokenFactory.token()
+            //     // ).to.equal(gauge.address);
+            //     //TODO: add more expects for added data
+            //     // console.log("lptoken %s ", lptoken.address);
+            //     console.log("\nbaseRewardPool %s ", (await setup.baseRewardPool.stakingToken()));
+
+            //     console.log("tokenFactory token %s ", (await tokenFactory.token()));
+            // });
+            // it("Calls earmarkRewards with existing pool number", async () => {
+            //     pid = 1;
+            //     await setup.controller.connect(root).earmarkRewards(pid);
+
+            //     expect(
+            //     ).to.equal(true);
+            // });
+        });
+    });
+  });
+});
--- test/test-init.js
+++ test/test-init.js
@@ -7,74 +7,54 @@ const initialize = async (accounts) => {
   setup.roles = {
     root: accounts[0],
     prime: accounts[1],
-    beneficiary: accounts[2],
-    buyer1: accounts[3],
-    buyer2: accounts[4],
-    buyer3: accounts[5],
-    buyer4: accounts[6],
+    staker: accounts[2],
+    reward_manager: accounts[3],
+    authorizer_adaptor: accounts[4],
+    operator: accounts[5],
   };
 
   return setup;
 };
 
-const getBAL = async (setup) => {
-  const Bal =  await ethers.getContractFactory("ERC20Mock", setup.roles.root);  //BAL 
-  const BAL_ADDRESS = await Bal.deploy("Bal", "BAL", decimals);
+const getTokens = async (setup) => {
+  const ERC20Factory =  await ethers.getContractFactory(
+    "ERC20Mock",
+    setup.roles.root
+  ); 
 
-  return { BAL_ADDRESS };
-};
-
-//(done)
-const getVoterProxy = async (setup) => {
-  const VoterProxy = await ethers.getContractFactory("VoterProxy", setup.roles.root);
-  const parameters = args ? args : [];
-  return await VoterProxy.deploy(...parameters);
-};
-//??? or this?? need VoterProxy contract to finish this
-// const VoterProxyProxyFactoryFactory = await ethers.getContractFactory(
-//   "VoterProxyProxyFactory",
-//   setup.roles.prime
-// );
-// const VoterProxyFactoryInstance =
-//   await VoterProxyFactoryFactory.deploy();
-
-// const proxy_tx = await VoterProxyProxyFactoryInstance
-//   .connect(setup.roles.prime)
-//   .createProxy(setup.VoterProxy.address, "0x00");
-// const proxy_receit = await proxy_tx.wait();
-// const proxy_addr = proxy_receit.events.filter((data) => {
-//   return data.event === PROXY_CREATION;
-// })[0].args["proxy"];
-// return await ethers.getContractAt("VoterProxy", proxy_addr);
-// };
-
-
-// const getContractInstance = async (factoryName, address, args) => {
-//   const Factory = await ethers.getContractFactory(factoryName, address);
-//   const parameters = args ? args : [];
-//   return await Factory.deploy(...parameters);
-// };
-
-//(done)
-const gettokenInstances = async (setup) => {
-  const D2DToken_Factory = await ethers.getContractFactory(
-    "D2DToken",
+  const VeBalFactory =  await ethers.getContractFactory(
+    "VeBalMock",
     setup.roles.root
   );
-  const decimals = 10; //10 only for example here
 
-  const D2DToken = await D2DToken_Factory.deploy(
-    decimals,
-    setup.roles.root.address
+  const D2DBalFactory = await ethers.getContractFactory(
+    "D2DBAL",
+    setup.roles.root
   );
 
-  // const PoolContract_Factory = await ethers.getContractFactory(
-  //   "PoolContract",
-  //   setup.roles.root
-  // );
-  const PoolContract = await ERC20_Factory.deploy("Pool Contract", "BALP", decimals);
+  const BAL = await ERC20Factory.deploy("Bal", "BAL");
 
-  return { D2DToken, PoolContract };
+  const D2DBal = await D2DBalFactory.deploy();
+
+  const PoolContract = await ERC20Factory.deploy("PoolToken", "BALP");
+  const WethBal = await ERC20Factory.deploy("WethBal", "WethBAL");
+  const VeBal = await VeBalFactory.deploy(WethBal.address, "VeBal", "VeBAL", setup.roles.authorizer_adaptor.address);
+
+  const GaugeControllerFactoty = await ethers.getContractFactory(
+    "GaugeControllerMock",
+    setup.roles.root
+  );         
+  //VotingEscrow = VeBal
+  const GaugeController = await GaugeControllerFactoty.deploy(BAL.address, VeBal.address);
+
+
+  const TokenFactoryFactory = await ethers.getContractFactory(
+    "TokenFactory",
+    setup.roles.root
+  );
+  const TokenFactory = await TokenFactoryFactory.deploy(setup.roles.operator.address);
+
+  return { BAL, D2DBal, PoolContract, WethBal, VeBal, GaugeController, TokenFactory };
 };
 
 const balDepositor = async (setup) => {
@@ -82,25 +62,24 @@ const balDepositor = async (setup) => {
     "BalDepositor",
     setup.roles.root
   );
-  const minterInstance = await ethers.getContract("D2DToken");
-  const staker = address();
-  const veBal = await ethers.getContract("veBal");
-  const escrow = veBal;
-  return await balDepositor.deploy(setup.roles.root.address, staker, minterInstance.address, escrow);
+  const wethBal = setup.tokens.WethBal;
+  const minter =  setup.tokens.D2DBal;
+  const staker =  setup.roles.staker;
+  const escrow =  setup.tokens.VeBal;
+
+  return await balDepositor.deploy(wethBal.address, staker.address, minter.address, escrow.address);
 };
 
-const baseRewardPool = async (setup) => {
-  const baseRewardPool = await ethers.getContractFactory(
-    "SignerV2",
-    setup.roles.root
-  );
-  const pid = 1; //1 for example (set correct later_)
-  const stakingTokenInstance = await ethers.getContract("D2DToken");
-  const rewardToken = BAL_ADDRESS;
-  const operator = await ethers.getContract("Controller");
-  const rewardManager = "0xedccb35798fae4925718a43cc608ae136208aa8d";
+const getVoterProxy = async (setup) => {
+  const VoterProxy = await ethers.getContractFactory("VoterProxy", setup.roles.root);
+  // const parameters = args ? args : [];
+  // return await VoterProxy.deploy(...parameters);
+  const mintr = setup.tokens.D2DBal;
+  const bal = setup.tokens.BAL;
+  const veBal = setup.tokens.VeBal;
+  const gaugeController = setup.tokens.GaugeController;
 
-  return await baseRewardPool.deploy(setup.roles.root.address, pid, stakingTokenInstance.address, rewardToken, operator.address, rewardManager);
+  return await VoterProxy.deploy(mintr.address, bal.address, veBal.address, gaugeController.address)    
 };
 
 const controller = async (setup) => {
@@ -108,18 +87,71 @@ const controller = async (setup) => {
     "Controller",
     setup.roles.root
   );
-  const staker = await ethers.getContract("ERC20Mock");
-  const minter = staker;
 
-  return await controller.deploy(setup.roles.root.address, staker.address, minter.address);
+  // const ERC20Mock_Factory =  await ethers.getContractFactory("ERC20Mock", setup.roles.root);  //BAL 
+  // const ERC20Mock = await ERC20Mock_Factory.deploy("ERC20Mock", "ERC20Mock");
+
+  // const staker = ERC20Mock;//await ethers.getContract("ERC20Mock");//await ethers.getContract("ERC20Mock");
+    //need to change staker mock as in addPool needed setStashAccess() function            
+    //IStaker(staker).setStashAccess(stash, true);
+  const wethBal = setup.tokens.WethBal;
+  const staker = setup.VoterProxy; //row 236 asks for VoterProxt function  IStaker(staker).setStashAccess(stash, true); //staker here if VoterProxy; staker from Booster 0x989aeb4d175e16225e39e87d0d97a3360524ad80 address
+  return await controller.deploy(staker.address, setup.roles.root.address, wethBal.address);
+};
+
+const baseRewardPool = async (setup) => {
+  const baseRewardPool = await ethers.getContractFactory(
+    "BaseRewardPool",
+    setup.roles.root
+  );
+  const pid = 1; // pool id
+  const stakingToken = setup.tokens.D2DBal;
+  const rewardToken = setup.tokens.BAL;
+  const operator = setup.controller;
+  const rewardManager = setup.roles.reward_manager;
+
+  return await baseRewardPool.deploy(pid, stakingToken.address, rewardToken.address, operator.address, rewardManager.address);
+};
+
+const rewardFactory = async (setup) => {
+  const RewardFactory = await ethers.getContractFactory(
+    "RewardFactory",
+    setup.roles.root
+  );
+
+  const bal = setup.tokens.BAL;
+  const operator = setup.controller;//roles.operator;
+
+  return await RewardFactory.deploy(operator.address, bal.address);
+};
+
+// const proxyFactory = async (setup) => {
+//   const ProxyFactory = await ethers.getContractFactory(
+//     "ProxyFactory",
+//     setup.roles.root
+//   );
+//   return await ProxyFactory.deploy();
+// };
+
+const stashFactory = async (setup) => {
+  const StashFactory = await ethers.getContractFactory(
+    "StashFactory",
+    setup.roles.root
+  );
+  const operator = setup.controller;
+  const rewardFactory = setup.rewardFactory;
+  const proxyFactory =  setup.VoterProxy; //must be .proxyFactory;
+  return await StashFactory.deploy(operator.address, rewardFactory.address, proxyFactory.address);
 };
 
 module.exports = {
   initialize,
-  getBAL,
   getVoterProxy,
-  gettokenInstances,
+  getTokens,
   balDepositor,
+  rewardFactory,
   baseRewardPool,
   controller,
+  stashFactory,
+  // proxyFactory
 };
